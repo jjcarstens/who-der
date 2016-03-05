d781959874881b53428d8c8994eabd7988bda2bb
Jon Carstens <jon.carstens@mx.com>
2016-03-05 15:07:51 -0700

    set trigger

--- lib/distance.rb
+++ lib/distance.rb
@@ -1,6 +1,16 @@
 require 'pi_piper'
 include PiPiper
 
+TRIGGER_GPIO = 23
+ECHO_GPIO = 24
+
+trigger = PiPiper::Pin.new(:pin => 23, :direction => :out)
+echo = PiPiper::Pin.new(:pin => 24, :direction => :in)
+
+trigger.on
+sleep 0.00001
+trigger.off
+
 watch :pin => 23 do
   puts "Pin changed from #{last_value} to #{value}"
 end
